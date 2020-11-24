e9bb18be3fe2ebc5d331b8728f6b9707ab945569
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-24 19:09:00 +0530

    [Kalyan] added methods for retriving data from the tables

---
+++ AddressBookModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookADO.NET
+{
+    /// <summary>
+    /// POCO class
+    /// </summary>
+    class AddressBookModel
+    {
+        public int PersonId { get; set; }
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int ContactTypeId { get; set; }
+        public string BookName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string email { get; set; }
+        public string bookName { get; set; }
+        public string contactType { get; set; }
+    }
+}
